59b8c40367420ce796bb83ed1989caf2c679db66
Cynthia Li <cynthia.lixinyi@berkeley.edu>
2024-04-12 02:05:53 -0700

    Start implementing frontend

--- client/recipe-generator/src/App.js
+++ client/recipe-generator/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+
+const RecipeCard = () => {
+  const [ingredients, setIngredients] = useState("");
+  const [mealType, setMealType] = useState("");
+  const [cuisine, setCuisine] = useState("");
+  const [cookingTime, setCookingTime] = useState("");
+  const [complexity, setComplexity] = useState("");
+
+  return (
+    <div className="w-[400px] border rounded-lg overflow-hidden shadow-lg">
+      <div className="px-6 py-4">
+      </div>
+    </div>
+  )
+
+
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
